d88fc6091ae1105e6a8594830c9f76b7e602e947
Darshana Agarwal <darshana170699@gmail.com>
2022-03-04 17:48:41 +0530

    the final docker

---
+++ src/main/java/com/app/final_docker/App.java
@@ -0,0 +1,13 @@
+package com.app.final_docker;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
